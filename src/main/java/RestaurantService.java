22fcafec357bfc76a23d43185884ead9945302e5
Satish Segu <SatishSegu946@gmail.com>
2021-10-20 07:50:24 +0530

    Part 2:Solution

--- src/main/java/RestaurantService.java
+++ src/main/java/RestaurantService.java
@@ -6,18 +6,17 @@ import java.util.List;
 public class RestaurantService {
     private static List<Restaurant> restaurants = new ArrayList<>();
 
-    public Restaurant findRestaurantByName(String restaurantName)throws restaurantNotFoundException{
+    public Restaurant findRestaurantByName(String restaurantName) throws restaurantNotFoundException {
 
         // Loop through the restaurants and find if the name mataches with any.
         // Here we are using equalsIgnoreCase for comparision so that users are not required to do exact search
-        for(Restaurant restaurant : restaurants){
-            if(restaurant.getName().equalsIgnoreCase(restaurantName)){
+        for (Restaurant restaurant : restaurants) {
+            if (restaurant.getName().equalsIgnoreCase(restaurantName)) {
                 return restaurant;
             }
         }
-        throw new restaurantNotFoundException("No restaurant found with the name "+restaurantName);
+        throw new restaurantNotFoundException("No restaurant found with the name " + restaurantName);
     }
-
 
     public Restaurant addRestaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
         Restaurant newRestaurant = new Restaurant(name, location, openingTime, closingTime);
